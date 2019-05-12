e0c256318c4e7a4387024ea944ba3e8445e528e0
Marshall Van Loon <marshall@Marshalls-MacBook-Pro.local>
2019-05-12 13:52:34 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## mhv2109's emacs config
+
+Based on flyingmachine's `emacs-for-clojure` repository found [here](https://github.com/flyingmachine/emacs-for-clojure).
+
+Updated with suggestions from aaronbedra's configuration found [here](http://aaronbedra.com/emacs.d/#languages).
+
+## Usage
+Clone repository into `~/.emacs.d` and start emacs.
---
+++ customizations/misc.el
@@ -0,0 +1,18 @@
+;; Changes all yes/no questions to y/n type
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; shell scripts
+(setq-default sh-basic-offset 2)
+(setq-default sh-indentation 2)
+
+;; No need for ~ files when editing
+(setq create-lockfiles nil)
+
+;; Go straight to scratch buffer on startup
+(setq inhibit-startup-message t)
+
+;; Save backups and autosaves to temp directory
+(setq backup-directory-alist
+  `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+  `((".*" ,temporary-file-directory t)))
