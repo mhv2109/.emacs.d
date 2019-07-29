3d9218fca6127e2d396b0427f3b82afb9b2830c8
Marshall Van Loon <marshall@Marshalls-MacBook-Pro.local>
2019-07-29 17:30:30 -0400

    disable company-mode for go and don't update path on each .go file load

--- customizations/setup-go.el
+++ customizations/setup-go.el
@@ -1,13 +1,22 @@
+(defvar gopath-initialized nil "Only set GOPATH and GOROOT once")
+
 (defun my-go-mode-hook ()
   ;; call gofmt before saving
   (add-hook 'before-save-hook 'gofmt-before-save)
   ;; autocomplete
   (auto-complete-mode 1)
   ;; set GOPATH and GOROOT from shell
-  (when (memq window-system '(mac ns x))
+  (setup-gopath)
+  ;; disable company-mode for go files
+  (company-mode -1))
+
+(defun setup-gopath ()
+  ;; set GOPATH and GOROOT from shell
+  (when (and (memq window-system '(mac ns x)) (not gopath-initialized))
     (exec-path-from-shell-copy-env "GOPATH")
     (exec-path-from-shell-copy-env "GOROOT")
-    (exec-path-from-shell-initialize)))
+    (exec-path-from-shell-initialize)
+    (setq gopath-initialized t)))
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 (with-eval-after-load 'go-mode
