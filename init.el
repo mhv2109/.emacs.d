74f7ba0b09dd056b2dd8f28b243e2b8ccc947dfc
mhv2109 <marshall.vanloon@gmail.com>
2024-11-19 08:31:18 -0500

    cider: enrich-classpath

--- init.el
+++ init.el
@@ -262,7 +262,9 @@
   :custom
   (sly-overlay-eval-result-duration nil))
 
-(use-package cider) ;; Clojure support: https://cider.mx/
+(use-package cider ;; Clojure support: https://cider.mx/
+  :custom
+  (cider-enrich-classpath t))
 
 (use-package paredit ;; Lisp programming conveniences: http://paredit.org/
   :hook ((emacs-lisp-mode lisp-mode lisp-interaction-mode clojure-mode cider-repl-mode) . paredit-mode))
