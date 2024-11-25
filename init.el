0bdc2995cf4050afffa1dd6f2a697df0c1e25c2d
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2024-11-25 14:53:58 -0500

    cider: re-enable cider-enrich-classpath

--- init.el
+++ init.el
@@ -277,7 +277,7 @@
 
 (use-package cider ;; Clojure support: https://cider.mx/
   :custom
-  ;; (cider-enrich-classpath t) ;; TODO: fix or allow to fail silently, doesn't work if network offline
+  (cider-enrich-classpath t) ;; Doesn't work offline -- can disable temporarily with `(setq cider-enrich-classpath nil)'
   (cider-font-lock-dynamically '(macro core function var)) ;; higlight all symbols that are known to be defined
   )
 
