c2bdabde1fc7dcca032a729a698fdae1a91f7da5
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2025-01-10 15:45:07 -0500

    elfeed: Fabrizio Ferri Benedetti's (passo.uno)

--- init.el
+++ init.el
@@ -713,6 +713,7 @@ otherwise add to start of list."
                   ("https://techcrunch.com/feed/" news tech)
                   ("https://www.localfirstnews.com/rss/" tech)
                   ("https://wiredream.com/atom.xml" tech)
+                  ("https://passo.uno/posts/index.xml" tech writing)
                   ("https://abcnews.go.com/abcnews/topstories" news)
                   ("https://feeds.bbci.co.uk/news/world/rss.xml" news)
                   ("https://moxie.foxnews.com/google-publisher/latest.xml" news)
