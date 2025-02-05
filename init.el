f782ef3f79558d1150d3ff7cec454b2b9a23cf11
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2025-02-05 10:20:32 -0500

    elfeed: add beej's blog

--- init.el
+++ init.el
@@ -732,6 +732,7 @@ otherwise add to start of list."
                   ("https://simonwillison.net/atom/everything/" tech ai)
                   ("https://qntm.org/rss.php" tech)
                   ("http://research.swtch.com/feed.atom" go tech)
+                  ("https://mastodon.sdf.org/@beejjorgensen.rss" tech)
                   ("https://abcnews.go.com/abcnews/topstories" news)
                   ("https://feeds.bbci.co.uk/news/world/rss.xml" news)
                   ("https://moxie.foxnews.com/google-publisher/latest.xml" news)
