38e5f04c70e71e1e6de18b1d1bea2ca9c6fd93cb
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2025-02-05 08:59:56 -0500

    elfeed: add Things of Interest

--- init.el
+++ init.el
@@ -730,6 +730,7 @@ otherwise add to start of list."
                   ("https://wiredream.com/atom.xml" tech)
                   ("https://passo.uno/posts/index.xml" tech writing)
                   ("https://simonwillison.net/atom/everything/" tech ai)
+                  ("https://qntm.org/rss.php" tech)
                   ("https://abcnews.go.com/abcnews/topstories" news)
                   ("https://feeds.bbci.co.uk/news/world/rss.xml" news)
                   ("https://moxie.foxnews.com/google-publisher/latest.xml" news)
