06c1988af986837bea8a7ce3465ef385b84bbb68
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2025-02-05 10:04:57 -0500

    elfeed: add Russ Cox's blog

--- init.el
+++ init.el
@@ -731,6 +731,7 @@ otherwise add to start of list."
                   ("https://passo.uno/posts/index.xml" tech writing)
                   ("https://simonwillison.net/atom/everything/" tech ai)
                   ("https://qntm.org/rss.php" tech)
+                  ("http://research.swtch.com/feed.atom" go tech)
                   ("https://abcnews.go.com/abcnews/topstories" news)
                   ("https://feeds.bbci.co.uk/news/world/rss.xml" news)
                   ("https://moxie.foxnews.com/google-publisher/latest.xml" news)
