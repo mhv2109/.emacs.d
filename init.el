1b8933f19fd592a3a0fa7c2b6d6eae23f6dd3f2f
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2025-02-25 15:19:47 -0500

    elfeed: add All Things Distributed

--- init.el
+++ init.el
@@ -720,6 +720,7 @@ otherwise add to start of list."
                   ("http://research.swtch.com/feed.atom" go tech)
                   ("https://mastodon.sdf.org/@beejjorgensen.rss" tech)
                   ("https://magazine.sebastianraschka.com/feed" tech ai)
+                  ("https://www.allthingsdistributed.com/atom.xml" tech)
                   ("https://abcnews.go.com/abcnews/topstories" news)
                   ("https://feeds.bbci.co.uk/news/world/rss.xml" news)
                   ("https://moxie.foxnews.com/google-publisher/latest.xml" news)
