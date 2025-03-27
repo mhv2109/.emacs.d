62587e4245607d0227e72e3528cdab0bdb38e948
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2025-03-27 11:26:30 -0400

    elfeed: add Rachel by the Bay

--- init.el
+++ init.el
@@ -722,6 +722,7 @@ otherwise add to start of list."
                   ("https://www.gilesthomas.com/feed/rss.xml" tech ai python)
                   ("https://sethmlarson.dev/feed" tech python)
                   ("https://commandcenter.blogspot.com/feeds/posts/default?alt=rss" tech go)
+                  ("https://rachelbythebay.com/w/atom.xml" tech)
                   ("https://abcnews.go.com/abcnews/topstories" news)
                   ("https://feeds.bbci.co.uk/news/world/rss.xml" news)
                   ("https://moxie.foxnews.com/google-publisher/latest.xml" news)
