c1592cbc90eca7999aeecb852664a5a024251fdd
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2025-04-23 13:15:32 -0400

    elfeed: add lobste.rs

--- init.el
+++ init.el
@@ -773,6 +773,7 @@ otherwise add to start of list."
                   ("https://www.seangoedecke.com/rss.xml" tech)
                   ("https://funcall.blogspot.com/feeds/posts/default" tech lisp)
                   ("https://blog.christianposta.com/feed.xml" tech)
+                  ("https://lobste.rs/rss" tech)
                   ("https://abcnews.go.com/abcnews/topstories" news)
                   ("https://feeds.bbci.co.uk/news/world/rss.xml" news)
                   ("https://moxie.foxnews.com/google-publisher/latest.xml" news)
