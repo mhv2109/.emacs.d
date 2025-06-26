bb43709e7de23f8f40f1e863b89ea1f10b0df880
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2025-06-25 22:47:48 -0400

    gptel: enable forced tool usage in GPTel configuration
    - Added `gptel-use-tools` with value `'force` to enforce tool usage
      in GPTel settings.
    - This change enhances the flexibility of GPTel's tool integration.

--- init.el
+++ init.el
@@ -725,6 +725,7 @@ otherwise add to start of list."
   (gptel-default-mode 'org-mode)
   (gptel-track-media t)
   (gptel-include-tool-results t)
+  (gptel-use-tools 'force)
   :config
   ;; configuration for making chat more legible: https://github.com/karthink/gptel?tab=readme-ov-file#additional-configuration
   (setf (alist-get 'org-mode gptel-prompt-prefix-alist) "* User:\n\n"
