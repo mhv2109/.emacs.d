88a72f1970875573a702101f85e226530e890cc9
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2025-07-07 11:49:37 -0400

    elfeed: add Github blog feed

--- init.el
+++ init.el
@@ -845,6 +845,7 @@ otherwise add to start of list."
                   ("https://blog.christianposta.com/feed.xml" tech)
                   ("https://lobste.rs/rss" tech)
                   ("https://antirez.com/rss" tech)
+                  ("https://github.blog/feed/" tech github)
                   ("https://abcnews.go.com/abcnews/topstories" news)
                   ("https://feeds.bbci.co.uk/news/world/rss.xml" news)
                   ("https://moxie.foxnews.com/google-publisher/latest.xml" news)
