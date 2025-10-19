ff293ede7b7992a926c9a48ce08c1fa8cceb6ebb
Marshall Van Loon <marshall.vanloon@gmail.com>
2025-10-18 22:04:46 -0400

    gptel+mcp:: Add GitHub MCP server configuration.

--- init.el
+++ init.el
@@ -802,6 +802,7 @@ otherwise add to start of list."
                      ("filesystem" . (:command "npx" :args ("-y" "@modelcontextprotocol/server-filesystem" ,(getenv "HOME")))) ;; grant filesystem access to home directory
                      ;; programming libraries, platforms, and tools
                      ("serena" . (:command "uvx" :args ("--from" "git+https://github.com/oraios/serena" "serena" "start-mcp-server" "--transport" "stdio" "--enable-web-dashboard" "false"))) ;; coding agent toolkit implemented as MCP server: https://github.com/oraios/serena (Docker image doesn't really work well, this MCP is blessed by Cybersecurity)
+                     ("github" . (:command "go" :args ("run" "github.com/github/github-mcp-server/cmd/github-mcp-server@latest" "stdio"))) ;; rquires go + GITHUB_PERSONAL_ACCESS_TOKEN env var
                      ("context7" . (:command "docker" :args ("run" "-i" "--rm"
                                                              "-e" "MCP_TRANSPORT=stdio" ;; use stdin/stdout vs HTTP API
                                                              "mcp/context7:latest"))) ;; Library docs
