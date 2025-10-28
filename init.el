670ac70cd0de077921ec93979c3aadcd5dbc18ad
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2025-10-28 12:45:15 -0400

    gptel+mcp: add atlassian MCP

--- init.el
+++ init.el
@@ -791,6 +791,7 @@ otherwise add to start of list."
                      ("aws" . (:command "docker" :args ("run" "-i" "--rm" "mcp/aws-documentation:latest"))) ;; AWS documentation
                      ("cloudflare" . (:command "npx" :args ("-y" "mcp-remote" "https://docs.mcp.cloudflare.com/sse"))) ;; Cloudflare docs (uses mcp-remote)
                      ("terraform" . (:command "docker" :args ("run" "-i" "--rm" "hashicorp/terraform-mcp-server:latest"))) ;; Terraform and registry docs
+                     ("atlassian" . (:command "npx" :args ("-y" "mcp-remote" "https://mcp.atlassian.com/v1/sse"))) ;; Access to Jira and Confluence
                      ))
   :config
   (require 'mcp-hub)
