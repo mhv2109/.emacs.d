244c1af1998e16c64d99395169d03fbc22a7180b
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2026-03-17 22:34:33 -0500

    elfeed: add james shore's blog

--- init.el
+++ init.el
@@ -977,6 +977,7 @@ all custom agents loaded from `gptel-agent-dirs'."
                   ("https://fedoramagazine.org/feed/" tech fedora)
                   ("https://ai.fedoraproject.org/feed/" tech ai fedora)
                   ("https://boristane.com/rss.xml" tech)
+                  ("https://www.jamesshore.com/v2/feed" tech)
                   ("https://abcnews.go.com/abcnews/topstories" news)
                   ("https://feeds.bbci.co.uk/news/world/rss.xml" news world)
                   ("https://moxie.foxnews.com/google-publisher/latest.xml" news)
